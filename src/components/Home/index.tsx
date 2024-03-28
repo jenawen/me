c96882dfdb3c8031e8a2b8c086e32d60006a23ef
jenawen <heartfilia12@gmail.com>
2024-03-28 15:17:14 -0700

    home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,20 @@
 import "./index.css"
+import me from '../../assets/homesvg.svg'
 export const HomePage = () => {
 
     return(
         <div className="home"> 
         
-        <div> My name is Jena.</div>
+        <img src={me}/>
+        <div className="title"> My name is Jena.</div>
         
-        
+        <div className="current">Credit One Bank - Operations</div>
+        <div className="desc">database management, content management systems, and customer support services </div>
+        <div className="links">
+            <div>about</div>
+            <div>projects</div>
+            <div>contact</div>
+        </div>
         </div>
     )
 }
