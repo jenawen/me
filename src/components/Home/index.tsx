7adc53d0e8ea28e662e0b27451b0e1afb713fac0
jenawen <heartfilia12@gmail.com>
2024-03-28 16:53:35 -0700

    home page done for now, start about page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,20 +1,34 @@
+
 import "./index.css"
 import me from '../../assets/homesvg.svg'
+import loc from '../../assets/location.svg'
+import { useState } from "react"
+import { About } from "../About"
 export const HomePage = () => {
+    const [isHomePage, setIsHomePage] = useState(true)
+    const [isAbout, setIsAbout] = useState(false)
+    const [isProject, setIsProject] = useState(false)
+    const [isContact, setIsContact] = useState(false)
 
     return(
-        <div className="home"> 
+        <>
+            {isHomePage ? (<>
+            <div className="home"> 
+                <img src={me}/>
+                <div className="title"> My name is Jena.</div>
         
-        <img src={me}/>
-        <div className="title"> My name is Jena.</div>
-        
-        <div className="current">Credit One Bank - Operations</div>
-        <div className="desc">database management, content management systems, and customer support services </div>
-        <div className="links">
-            <div>about</div>
-            <div>projects</div>
-            <div>contact</div>
-        </div>
-        </div>
+                <div className="current"><img src={loc} className="locicon"/>Credit One Bank - Operations</div>
+                <div className="desc">database management, content management systems, and customer support services </div>
+                <div className="links">
+                    <div onClick={()=>{
+                        setIsHomePage(false)
+                        setIsAbout(true)
+                    }}>about</div>
+                    <div>projects</div>
+                    <div>contact</div>
+                </div>
+            </div>
+        </>) : isAbout ? (<><About/></>) : (<></>)}
+    </>
     )
 }
